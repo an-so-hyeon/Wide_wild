05796aaae71552c13c0169b82ec28fe2f78a2966
안소현 <93846012+an-so-hyeon@users.noreply.github.com>
2022-09-13 15:57:26 +0900

    Update gugudan.py

--- sw02/gugudan.py
+++ sw02/gugudan.py
@@ -1,7 +1,10 @@
-def main():
-    pass def gugudan(dan):    
+
+def gugudan(dan):    
     for i in range(1, 10):
         print('{} * {} = {}'.format(dan, i, dan * i))
+
+def main():
+    gugudan(5)
         
 if __main__ == '__main__":
-main()
+    main()
